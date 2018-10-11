233bfefe652fff14982489a7b36b606fd4f74e92
Woshiajuana <zhigang.chen@owulia.com>
2018-10-11 16:33:29 +0800

    更新store.plugin.js

--- source/plugins/store.plugin.js
+++ source/plugins/store.plugin.js
@@ -20,13 +20,23 @@ export default {
 
     // 存储
     set: (key, value) => new Promise((resolve, reject) => {
-        wx.setStorageSync(key, value);
+        let reg_key = storeConfig[key];
+        if (!reg_key)
+            return reject(`${key} is no register`);
+        wx.setStorageSync(reg_key, value);
         return resolve(value);
     }),
 
     // 删除
     remove: (key) => new Promise((resolve, reject) => {
-        wx.removeStorageSync(key);
+        let reg_key = storeConfig[key];
+        wx.removeStorageSync(reg_key);
+        return resolve();
+    }),
+
+    // 清除
+    clear: () => new Promise((resolve, reject) => {
+        wx.clearStorageSync();
         return resolve();
     }),
 }
