54e0eb3456d56788e6cf8aa2265015285e14924e
Woshiajuana <zhigang.chen@owulia.com>
2018-10-12 13:42:31 +0800

    更新页面

--- example/build/component.wow.wx.loader.js
+++ example/build/component.wow.wx.loader.js
@@ -5,6 +5,14 @@ let _loaderUtils = require('loader-utils');
 module.exports = function (content) {
     let options = (0, _loaderUtils.getOptions)(this) || {};
     let { arr_source, source } = content;
-    console.log(source);
+    let { output, entry } = options;
+    let use_components_obj = JSON.parse(source).usingComponents;
+    let use_components = [];
+    if (use_components_obj) {
+        for (let key in use_components_obj) {
+            use_components.push(use_components_obj[key]);
+        }
+    }
+    console.log(use_components);
     return content;
 };
