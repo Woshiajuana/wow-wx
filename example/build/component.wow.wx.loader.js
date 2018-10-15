360d063942bfc53f92e282b398865a35b1ccf8ac
Woshiajuana <zhigang.chen@owulia.com>
2018-10-15 09:49:23 +0800

    更新页面数据

--- example/build/component.wow.wx.loader.js
+++ example/build/component.wow.wx.loader.js
@@ -1,6 +1,5 @@
 let Path = require('path');
 let fs = require('fs');
-let fs_extra = require('fs-extra');
 let _loaderUtils = require('loader-utils');
 
 module.exports = function (content) {
@@ -9,22 +8,7 @@ module.exports = function (content) {
     let { output, entry } = options;
     let obj_source = JSON.parse(source);
     let usingComponents = obj_source.usingComponents || {};
-    let useComponents = obj_source.useComponents;
     let use_components = [];
-    if (useComponents) {
-        usingComponents = {
-            ...usingComponents,
-            ...useComponents,
-        };
-        for (let key in useComponents) {
-            let value = useComponents[key];
-            value = value.substring(value.indexOf('/components'));
-            !obj_source.usingComponents && (obj_source.usingComponents = {});
-            obj_source.usingComponents[key] = value;
-        }
-        console.log(this.resource);
-        fs_extra.writeFileSync(this.resource, JSON.stringify(obj_source, null, 4));
-    }
     console.log(obj_source);
     if (usingComponents) {
         for (let key in usingComponents) {
