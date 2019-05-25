45f3c054c3d0f26069c18f4ec495a1fa17815a8a
Woshiajuana <zhigang.chen@owulia.com>
2019-05-25 18:34:33 +0800

    tijiao

--- source/index.js
+++ source/index.js
@@ -1,4 +1,10 @@
 
-export {
+import App                          from './lib/app'
+import Page                         from './lib/page'
+import Component                    from './lib/component'
 
+export {
+    App,
+    Page,
+    Component,
 }
