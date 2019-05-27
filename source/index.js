56fb52d9d81a88d47bee4889babdbb9a6a05c8da
Woshiajuana <zhigang.chen@owulia.com>
2019-05-27 10:44:13 +0800

    sub test

--- source/index.js
+++ source/index.js
@@ -1,10 +1,18 @@
 
-import WowApp                       from './lib/app'
-import WowPage                      from './lib/page'
-import WowComponent                 from './lib/component'
+// import WowApp                       from './lib/app'
+// import WowPage                      from './lib/page'
+// import WowComponent                 from './lib/component'
+//
+// export {
+//     WowApp,
+//     WowPage,
+//     WowComponent,
+// }
 
-export {
-    WowApp,
-    WowPage,
-    WowComponent,
-}
+import Generate                         from './utils/generate.util'
+
+let options = Generate.generateAppOptions({
+
+});
+
+console.log(options);
