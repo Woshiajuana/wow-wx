76014e2b2e0374770fae89005a8b84171dc58b0b
Woshiajuana <zhigang.chen@owulia.com>
2019-05-27 10:18:25 +0800

    sub code

--- source/utils/generate.util.js
+++ source/utils/generate.util.js
@@ -6,6 +6,8 @@ export const generateAppOptions = (options) => {
     } = options;
     if (!mixins || !mixins.length)
         return options;
+    let keys = ['onLaunch', 'onShow', 'onHide', 'onError'];
+    let target = initTarget();
     delete options.mixins;
     if (!data)
         data = {};
@@ -15,12 +17,46 @@ export const generateAppOptions = (options) => {
         if (mixin.data)
             Object.assign(mixinData, mixin.data);
         delete mixin.data;
+        mixinTarget(target, mixin, keys);
         Object.assign(mixinOption, mixins);
     });
+    mixinTarget(target, options, keys);
     options = Object.assign(mixinOption, options);
     options.data = Object.assign(mixinData, data);
+    generateExecutableFn(options, target);
     return options;
 };
+
+
+
+function initTarget (keys) {
+    let target = {};
+    keys.forEach((key) => {
+        target[key] = [];
+    });
+    return target;
+}
+
+function mixinTarget (target, source, keys) {
+    for (let k in source) {
+        if (keys.indexOf(k) > -1) {
+            let fn = source[k];
+            target[k].push(fn);
+        }
+    }
+}
+
+function generateExecutableFn (target, source) {
+    for (let k in target) {
+        source[k] = function (options) {
+            target[k].forEach((fn) => {
+                fn(options);
+            });
+        }
+    }
+    return source;
+}
+
 
 export default {
     generateAppOptions,
