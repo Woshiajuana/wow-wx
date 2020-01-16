0c9978eb8252d92bfc31da9d06b6d7a95bf548f1
Woshiajuana <zhigang.chen@owulia.com>
2020-01-16 18:14:45 +0800

    subcode code

---
+++ source/mixins/validate.mixin.js
@@ -0,0 +1,9 @@
+
+const Validate = require('../utils/validate.util');
+
+export default {
+    validateCheck: Validate.check,
+    validateInput: Validate.input,
+    validateAssignment: Validate.assignment,
+    validateFilterNull: Validate.filterNull,
+}
---
+++ source/utils/source.util.js
@@ -0,0 +1,2 @@
+
+module.exports = (name) => `${getApp().wow$.configs.Env.SOURCE_BASE_URL}image/${name}`;
