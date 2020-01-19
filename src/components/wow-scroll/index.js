d7eacb10df91e6fb4d1d9f0957e513c647293e46
Woshiajuana <zhigang.chen@owulia.com>
2020-01-19 10:41:44 +0800

    subcode code wow-scroll

---
+++ src/components/wow-scroll/index.js
@@ -0,0 +1,28 @@
+import './index.json'
+import './index.wxml'
+import './index.scss'
+
+import WowComponent from 'source/lib/component'
+
+new WowComponent({
+
+    mixins: [
+        WowComponent.wow$.mixins.Text,
+    ],
+    lifetimes: {
+        attached () {
+            console.log('加载测试组件 => 成功');
+        },
+    },
+    methods: {
+        handleScroll (event) {
+            console.log('滚动距离 =>', event);
+        },
+        handleTouchStart (event) {
+            console.log('滚动开始 =>', event);
+        },
+        handleTouchEnd (event) {
+            console.log('滚动结束 =>', event);
+        },
+    },
+});
