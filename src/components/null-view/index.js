b97ae8982644fd9cbfe24478057d0a41a072057a
Woshiajuana <zhigang.chen@owulia.com>
2020-01-28 20:12:38 +0800

    subcode code

--- src/components/null-view/index.js
+++ src/components/null-view/index.js
@@ -4,14 +4,4 @@ import './index.scss'
 
 import WowComponent from 'source/lib/component'
 
-new WowComponent({
-
-    mixins: [
-        WowComponent.wow$.mixins.Text,
-    ],
-    lifetimes: {
-        attached () {
-            console.log('加载测试组件 => 成功');
-        },
-    },
-});
+new WowComponent();
