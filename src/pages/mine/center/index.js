695b96633b92006481e2f7ec8ce92331d21ae89b
Woshiajuana <zhigang.chen@owulia.com>
2020-01-29 19:34:01 +0800

    subcode code

--- src/pages/mine/center/index.js
+++ src/pages/mine/center/index.js
@@ -8,15 +8,26 @@ import WowPage from 'source/lib/page'
 new WowPage({
     mixins: [
         WowPage.wow$.mixins.Modal,
+        WowPage.wow$.mixins.Input,
+        WowPage.wow$.mixins.Router,
+        WowPage.wow$.mixins.Jump,
     ],
     data: {
         objEntry: {
-            avatar: { label: '头像', value: '', useMargin: true },
+            avatar: { label: '头像', value: '', useMargin: true, fn: 'handleAvatar' },
             nickname: { label: '昵称', value: '我是阿倦啊', useMargin: true, url: '' },
             email: { label: '邮箱', value: '979703986@qq.com', useMargin: false, url: '' },
             sex: { label: '性别', value: '男', useMargin: true, url: '' },
             signature: { label: '签名', value: '好好学习，天天向上！', useMargin: false, url: '' },
             qr: { label: '名片', value: '', useMargin: false, url: '' },
         },
+    },
+    handleAvatar () {
+        this.modalActionSheet([
+            '相册',
+            '拍照',
+        ]).then((res) => {
+            console.log(res);
+        }).null();
     },
 });
