ab5d854cc1b12973a311052ccd46dec355d18e67
Woshiajuana <zhigang.chen@owulia.com>
2020-01-29 20:25:05 +0800

    subcode

--- src/pages/mine/nickname/index.js
+++ src/pages/mine/nickname/index.js
@@ -8,11 +8,14 @@ import WowPage from 'source/lib/page'
 new WowPage({
     mixins: [
         WowPage.wow$.mixins.Modal,
+        WowPage.wow$.mixins.Input,
     ],
     data: {
-        nickname: '',
+        nickname: '12',
     },
     handleInput (event) {
+        let data = this.inputParams(event);
+        console.log('触发了 =>', data);
         console.log('触发了 =>', event);
     },
 });
