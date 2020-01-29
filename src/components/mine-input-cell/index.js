4288de0657b2b3842c6218c846d9e1a7be26ba27
Woshiajuana <zhigang.chen@owulia.com>
2020-01-29 20:20:57 +0800

    subcode code

--- src/components/mine-input-cell/index.js
+++ src/components/mine-input-cell/index.js
@@ -6,11 +6,40 @@ import WowComponent from 'source/lib/component'
 
 new WowComponent({
     mixins: [
-        WowComponent.wow$.mixins.Text,
+        WowComponent.wow$.mixins.Input,
     ],
-    lifetimes: {
-        attached () {
-            console.log('加载测试组件 => 成功');
+    options: {
+        multipleSlots: true // 在组件定义时的选项中启用多slot支持
+    },
+    externalClasses: ['class-button'],
+    data: {
+        numLen: 0,
+    },
+    properties: {
+        value: {
+            type: String,
+            value: '',
+        },
+        placeholder: {
+            type: String,
+            value: '请输入',
+        },
+        prompt: {
+            type: String,
+            value: '',
+        },
+        maxLength: {
+            type: Number,
+            value: 20,
+        }
+    },
+    methods: {
+        handleInput (event) {
+            let { value } = this.inputParams(event);
+            this.triggerEvent('input', { value });
+        },
+        handleClose () {
+            this.triggerEvent('input', { value: 0 });
         },
     },
 });
