99d3cb19cf442974e8732218c882ee3bdff21f04
Woshiajuana <zhigang.chen@owulia.com>
2020-01-29 19:18:03 +0800

    subcode code

--- src/components/wow-cell/index.js
+++ src/components/wow-cell/index.js
@@ -5,12 +5,15 @@ import './index.scss'
 import WowComponent from 'source/lib/component'
 
 new WowComponent({
+    options: {
+        multipleSlots: true // 在组件定义时的选项中启用多slot支持
+    },
     properties: {
-        cellLabel: { // 最大距离
+        cellLabel: {
             type: String,
             value: ''
         },
-        cellValue: { // 最小距离
+        cellValue: {
             type: String,
             value: ''
         },
@@ -21,6 +24,14 @@ new WowComponent({
         useMargin: {
             type: Boolean,
             value: false,
+        },
+        cellHeight: {
+            type: Number,
+            value: 100,
+        },
+        useArrow: {
+            type: Boolean,
+            value: true,
         },
     },
 });
