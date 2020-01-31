9cfc8b77e79f3fc3c7545e9c64816ea91d86a594
Woshiajuana <zhigang.chen@owulia.com>
2020-01-31 21:18:20 +0800

    subcode code

--- src/pages/mine/center/index.js
+++ src/pages/mine/center/index.js
@@ -14,12 +14,46 @@ new WowPage({
     ],
     data: {
         objEntry: {
-            avatar: { label: '头像', value: '', useMargin: true, fn: 'handleAvatar' },
-            nickname: { label: '昵称', value: '我是阿倦啊', useMargin: true, url: 'mine_nickname_index' },
-            email: { label: '邮箱', value: '979703986@qq.com', useMargin: false, url: 'mine_email_index' },
-            sex: { label: '性别', value: '男', useMargin: true, url: 'mine_sex_index' },
-            signature: { label: '签名', value: '好好学习，天天向上！', useMargin: false, url: 'mine_signature_index' },
-            qr: { label: '名片', value: '', useMargin: false, url: 'mine_qrcode_index' },
+            avatar: {
+                label: '头像',
+                value: '',
+                useMargin: true,
+                fn: 'handleAvatar'
+            },
+            nickname: {
+                label: '昵称',
+                value: '我是阿倦啊',
+                useMargin: true,
+                url: 'mine_nickname_index'
+            },
+            email: {
+                label: '邮箱',
+                value: '979703986@qq.com',
+                useMargin: false,
+                url: 'mine_email_index'
+            },
+            sex: {
+                label: '性别',
+                value: '男',
+                useMargin: true,
+                url: 'mine_sex_index',
+                options: [
+                    { label: '男', value: 1 },
+                    { label: '女', value: 2 },
+                ],
+            },
+            signature: {
+                label: '签名',
+                value: '好好学习，天天向上！',
+                useMargin: false,
+                url: 'mine_signature_index'
+            },
+            qr: {
+                label: '名片',
+                value: '',
+                useMargin: false,
+                url: 'mine_qrcode_index'
+            },
         },
     },
     handleAvatar () {
@@ -29,5 +63,8 @@ new WowPage({
         ]).then((res) => {
             console.log(res);
         }).null();
+    },
+    handleChange (event) {
+        console.log(event);
     },
 });
