cc0aed91b2a49ef03dd1b6578e8cd1eda9f1e104
Woshiajuana <zhigang.chen@owulia.com>
2020-02-02 20:19:06 +0800

    subcode

--- cmd/webpack.config.js
+++ cmd/webpack.config.js
@@ -179,7 +179,28 @@ const config = {
                         }
                     },
                 ]
-            }
+            },
+            {
+                test: /\.(png|jpeg|jpg|gif|svg)$/,
+                exclude: /node_modules/,
+                use: [
+                    {
+                        loader: './cmd/copy.wow.wx.loader.js',
+                        options: {
+                            output: rootOutDirectoryAbsolutePath,
+                            entry: rootDirectoryAbsolutePath,
+                        }
+                    },
+                    {
+                        loader: './cmd/resources.wow.wx.loader.js',
+                        options: {
+                            use_source: true,
+                            use_image: false,
+                            use_filter: [],
+                        }
+                    },
+                ]
+            },
         ]
     },
     node: {
