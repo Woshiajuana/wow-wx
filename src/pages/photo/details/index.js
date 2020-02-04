e88f5f799cc907977c15016bc8659daf9df72645
Woshiajuana <zhigang.chen@owulia.com>
2020-02-04 11:05:14 +0800

    subcode

--- src/pages/photo/details/index.js
+++ src/pages/photo/details/index.js
@@ -8,29 +8,5 @@ import WowPage from 'source/lib/page'
 new WowPage({
     mixins: [
         WowPage.wow$.mixins.Modal,
-        WowPage.wow$.mixins.Refresh,
     ],
-    data: {
-        arrEntry: [
-            { label: '照片', class: 'icon-zhaopian_huabanfuben', useMargin: true, url: '' },
-            { label: '收藏', class: 'icon-shoucang-tianchong', useMargin: true, url: '' },
-            { label: '历史', class: 'icon-3lishi', useMargin: false, url: '' },
-            { label: '设置', class: 'icon-shezhi', useMargin: true, url: '' },
-        ]
-    },
-    onLoad(options) {
-        console.log('首页加载 => ', options);
-        console.log('首页执行wow$ =>', this.wow$);
-    },
-    handleTap () {
-        let { Modal } = this.wow$.plugins;
-        // this.modalToast('11111');
-        // Modal.toast(1);
-        // console.log(new Promise(()=> {}).toast);
-
-        this.testPromise().toast();
-    },
-    testPromise () {
-        return Promise.reject('xxx');
-    }
 });
