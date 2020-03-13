7d433d5f76b18514783568f9300043fd8411777c
Woshiajuana <zhigang.chen@owulia.com>
2020-03-13 14:55:37 +0800

    subcode code

--- src/components/photo-list-item/index.js
+++ src/components/photo-list-item/index.js
@@ -7,6 +7,8 @@ import WowComponent from 'source/lib/component'
 new WowComponent({
     mixins: [
         WowComponent.wow$.mixins.Input,
+        WowComponent.wow$.mixins.Router,
+        WowComponent.wow$.mixins.Jump,
     ],
     properties: {
         data: { // 推荐
@@ -16,6 +18,14 @@ new WowComponent({
         mode: {
             type: String,
             value: 'manage',
+        },
+    },
+    methods: {
+        handleFireFn () {
+            let { mode, data } = this.data;
+            if (mode === 'info')
+                return this.triggerEvent('click', data);
+            this.routerPush('photo_info_index', data);
         },
     },
 });
