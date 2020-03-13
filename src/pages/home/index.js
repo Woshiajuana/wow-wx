328a172f801915bd1c5ef49b80c20fe3d2fa67f5
Woshiajuana <zhigang.chen@owulia.com>
2020-03-13 16:54:04 +0800

    subcode code

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -7,21 +7,75 @@ import WowPage from 'source/lib/page'
 
 new WowPage({
     mixins: [
-        WowPage.wow$.mixins.Modal,
         WowPage.wow$.mixins.Input,
+        WowPage.wow$.mixins.Modal,
         WowPage.wow$.mixins.Router,
         WowPage.wow$.mixins.Jump,
+        WowPage.wow$.mixins.Refresh,
     ],
     data: {
+        numCurIndex: 0,
         arrTab: [
-            { label: '推荐', active: true },
-            { label: '关注', active: false },
+            {
+                label: '推荐',
+                limit: 10,
+                arrData: '',
+                url: 'REQ_PHOTO_RECOMMEND',
+                isLoading: false,
+            },
+            {
+                label: '关注',
+                limit: 10,
+                arrData: '',
+                url: 'REQ_PHOTO_RECOMMEND',
+                isLoading: false,
+            },
         ],
+    },
+    onLoad (options) {
+        this.routerGetParams(options);
+        this.assignmentData();
+        this.reqDataList();
+    },
+    assignmentData () {
+        let { key } = this.data.params$;
+        this.setData({ numCurIndex: key === 'objInfo.numFollowing' ? 1 : 0 });
+    },
+    reqDataList (callback) {
+        let { arrTab, numCurIndex } = this.data;
+        let { url, limit, arrData } = arrTab[numCurIndex];
+        let { Http } = this.wow$.plugins;
+        arrTab[numCurIndex].isLoading = true;
+        this.setData({ arrTab });
+        Http(Http.API[url], {
+            limit,
+        }, {
+            loading: typeof callback !== 'function',
+        }).then((res) => {
+            if (!arrData) arrData = [];
+            arrTab[numCurIndex].arrData = typeof callback === 'function' ? [ ...res, ...arrData ] : [ ...arrData, ...res ];
+        }).toast().finally(() => {
+            arrTab[numCurIndex].isLoading = false;
+            this.setData({ arrTab });
+            console.log(arrTab)
+            typeof callback === 'function' && callback();
+        });
+    },
+    handleRefresh (callback) {
+        this.reqDataList(callback);
     },
     handleTabSwitch (event) {
         let { index } = this.inputParams(event);
-        let { arrTab } = this.data;
-        arrTab.forEach((item, i) => item.active = index === i);
-        this.setData({ arrTab });
+        this.setData({ numCurIndex: index });
+        if (this.data.arrTab[index].arrData.length === 0) {
+            this.reqDataList();
+        }
+    },
+    handleLoad (event) {
+        let { arrTab, numCurIndex } = this.data;
+        let { isLoading } = arrTab[numCurIndex];
+        if (isLoading)
+            return console.log('数据正在加载中');
+        this.reqDataList();
     },
 });
