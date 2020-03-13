df89fbbadcf2b3353d3c47afb2ef4790e1a98b55
Woshiajuana <zhigang.chen@owulia.com>
2020-03-13 15:27:10 +0800

    subcode code

--- src/pages/friend/index.js
+++ src/pages/friend/index.js
@@ -7,19 +7,96 @@ import WowPage from 'source/lib/page'
 
 new WowPage({
     mixins: [
-        WowPage.wow$.mixins.Modal,
         WowPage.wow$.mixins.Input,
+        WowPage.wow$.mixins.Modal,
+        WowPage.wow$.mixins.Router,
+        WowPage.wow$.mixins.Jump,
     ],
     data: {
-        arrData: [
-            { label: '我的粉丝', active: true, data: 10 },
-            { label: '我的关注', active: false, data: 20 },
+        numCurIndex: 0,
+        arrTab: [
+            {
+                label: '我的粉丝',
+                numIndex: 1,
+                numSize: 10,
+                arrData: '',
+                numTotal: 0,
+                url: 'REQ_FOLLOWER_LIST',
+            },
+            {
+                label: '我的关注',
+                numIndex: 1,
+                numSize: 10,
+                arrData: '',
+                numTotal: 0,
+                url: 'REQ_FOLLOWING_LIST',
+            },
         ],
+    },
+    onLoad () {
+        this.reqDataList();
+    },
+    reqDataList () {
+        let { arrTab, numCurIndex } = this.data;
+        let { url, numIndex, numSize, arrData } = arrTab[numCurIndex];
+        let { Http } = this.wow$.plugins;
+        Http(Http.API[url], {
+            numIndex,
+            numSize,
+        }).then((res) => {
+            let { list = [], total } = res || {};
+            arrTab[numCurIndex].arrData = numIndex === 1 ? list : [ ...arrData, ...list ];
+            arrTab[numCurIndex].numTotal = total;
+            this.setData({ arrTab });
+        }).toast();
+    },
+    handleRefresh () {
+        let { arrTab, numCurIndex } = this.data;
+        arrTab[numCurIndex].numIndex = 1;
+        this.setData({ arrTab });
+        this.reqDataList();
     },
     handleTabSwitch (event) {
         let { index } = this.inputParams(event);
-        let { arrData } = this.data;
-        arrData.forEach((item, i) => item.active = index === i);
-        this.setData({ arrData });
+        this.setData({ numCurIndex: index });
+        if (this.data.arrTab[index].arrData.length === 0) {
+            this.reqDataList();
+        }
     },
+    handleLoad (event) {
+        let { arrTab, numCurIndex } = this.data;
+        let { numIndex, numTotal, arrData } = arrTab[numIndex];
+        if (arrData.length >= numTotal)
+            return console.log('数据加载已完毕');
+        arrTab[numCurIndex].numIndex++;
+        this.setData({ arrTab });
+        this.reqDataList();
+    },
+    // 取消供应收藏
+    handleSupplyCancelCollect (event) {
+        let { item } = this.inputParams(event);
+        this.modalConfirm(`确认取消该供应收藏么?`).then(() => {
+            let { Http } = this.wow$.plugins;
+            Http(Http.API.DO_SUPPLY_COLLECT_DEL, {
+                Id: item.Id,
+            });
+        }).then(() => {
+            this.modalToast('取消成功');
+            this.handleRefresh();
+        }).toast();
+    },
+    // 取消店铺收藏
+    handleStoreCancelCollect (event) {
+        let { item } = this.inputParams(event);
+        this.modalConfirm(`确认取消该店铺收藏么?`).then(() => {
+            let { Http } = this.wow$.plugins;
+            return Http(Http.API.DO_STORE_COLLECT_DEL, {
+                Id: item.ShopId,
+                Operation: '2',
+            });
+        }).then(() => {
+            this.modalToast('取消成功');
+            this.handleRefresh();
+        }).toast();
+    }
 });
