b38551756e980cd480e2e0e7c11b166eb99b747d
Woshiajuana <zhigang.chen@owulia.com>
2020-03-16 13:45:26 +0800

    subcode code

--- src/pages/user/index.js
+++ src/pages/user/index.js
@@ -15,37 +15,22 @@ new WowPage({
         WowPage.wow$.mixins.Validate,
         WowPage.wow$.mixins.User,
     ],
-    data: {
-        arrEntry: [
-            { label: '照片', icon: 'icon-zhaopian_huabanfuben', useMargin: true, url: 'photo_index' },
-            { label: '收藏', icon: 'icon-shoucang-tianchong', useMargin: true, url: 'collect_index' },
-            { label: '历史', icon: 'icon-3lishi', useMargin: false, url: 'history_index' },
-            { label: '设置', icon: 'icon-shezhi', useMargin: true, url: 'setting_index' },
-        ],
-        objNotice: { label: '消息', url: 'notice_index' },
-        objInfo: {
-            numFollower: { value: 0, label: '粉丝', url: 'friend_index', key: 'objInfo.numFollower' },
-            numFollowing: { value: 0, label: '关注', url: 'friend_index', key: 'objInfo.numFollowing' },
-            numPhoto: { value: 0, label: '照片', url: 'photo_index', key: 'objInfo.numPhoto' },
-        },
-    },
-    onShow () {
-        this.userGet().then(this.reqUserInfo.bind(this)).null();
+    onLoad (options) {
+        this.routerGetParams(options);
+        this.reqUserInfo();
     },
     handleRefresh (callback) {
         this.reqUserInfo(callback);
     },
     reqUserInfo (callback) {
+        let { _id: id } = this.data.params$;
         let { Http } = this.wow$.plugins;
-        Http(Http.API.REQ_USER_INFO, {}, {
+        Http(Http.API.REQ_USER_INFO, {
+            id,
+        }, {
             loading: false,
         }).then((res) => {
-            return this.userUpdate(res);
-        }).then(() => {
-            return this.userGet();
-        }).then(() => {
-            let { objInfo, user$ } = this.data;
-            this.validateAssignment(this, user$, objInfo, 'objInfo');
+
         }).toast().finally(() => {
             typeof callback === 'function' && callback();
         });
