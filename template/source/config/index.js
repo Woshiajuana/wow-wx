db5979b9d7d238e71b704be5af0b0479d41fddda
Woshiajuana <zhigang.chen@owulia.com>
2020-10-26 16:59:48 +0800

    subcode

--- template/source/config/index.js
+++
@@ -1,8 +0,0 @@
-
-import Config from 'src/config/wow-wx.config'
-
-export default Object.assign({
-    modalFormatMsg (options) {
-        return options.errMsg || options.msg || options.message || JSON.stringify(options);
-    }
-}, Config);
--- template/source/index.js
+++
@@ -1,4 +0,0 @@
-
-export default from './lib/app'
-export default from './lib/page'
-export default from './lib/component'
