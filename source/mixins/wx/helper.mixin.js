58aff2894b9f4b07e381e97a2a3cd114d8e59557
chenzhigang <979703986@qq.com>
2022-05-13 14:41:58 +0800

    subcode

--- source/mixins/wx/helper.mixin.js
+++ source/mixins/wx/helper.mixin.js
@@ -4,13 +4,14 @@ export default {
 
     /**
      * 将 wx 的方法扩展成 Promise，方便处理
-     * @param fn {String}
+     * @param fn {String | Function}
      * @param options {Object}
      * @return Promise
      * */
     helperFnPromise (fn, options = {}) {
         return new Promise(((resolve, reject) => {
-            wx[fn]({
+            fn = typeof fn === 'function' ? fn : wx[fn]
+            fn({
                 ...options,
                 success: resolve,
                 fail: reject,
